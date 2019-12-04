e34356b7a48cdd942038328247d395674acafcfe
Cloud User <centos@ip-172-31-83-21.ec2.internal>
2019-12-04 13:51:28 -0500

    changes to submit_all_jobs

--- jwst-cal/submit_all_jobs.py
+++ jwst-cal/submit_all_jobs.py
@@ -17,18 +17,22 @@ with open('./s3_data_list.txt','r') as f:
         jobfile=f'{jobdir}/{dataset}.job'
         #print(jobfile)
         with open(jobfile,'w') as job:
-            job.write('Executable=/home/ec2-user/conda/envs/jwst/bin/strun\n')
-            job.write(f'Arguments=jwst.pipeline.Detector1Pipeline s3://dmd-test-condor-prototype-data/input/{datafile} --output_dir s3://dmd-test-condor-prototype-data/output\n')
-            job.write('\n')
-            job.write('Universe=vanilla\n')
-            job.write(f'Log={dataset}.condor_log\n')
-            job.write(f'+Out = "ALOG_{dataset}.out"\n')
-            job.write(f'+Err = "ALOG_{dataset}.err"\n')
-            job.write(f'+Instances=1\n')
-            job.write('getenv=True\n')
-            job.write('Notification=Never\n')
-            job.write('Queue')
-            
+            job.write(f"""
+Executable=/bin/bash
+Arguments=strun_wrapper.sh  jwst.pipeline.Detector1Pipeline s3://dmd-test-condor-prototype-data/input/{datafile} --output_dir s3://dmd-test-condor-prototype-data/output
+
+Universe=vanilla
+Log={dataset}.condor_log
++Out = "ALOG_{dataset}.out"
++Err = "ALOG_{dataset}.err"
++Instances=1
+getenv=True
+transfer_executable=false
+transfer_input_files=strun_wrapper.sh
+Notification=Never
+Queue
+""")
+        os.system(f"cp strun_wrapper.sh {jobdir}")
         os.chdir(jobdir)
         cmd = f'condor_submit {dataset}.job'
         #print(cmd)
